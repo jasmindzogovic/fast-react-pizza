6a1a41289054ce6abb4c5eb23b503ab302af24c0
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-04 12:01:40 -0400

    created initial project scaffolding

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza</Link>
+      <p>Jasmin</p>
+    </header>
+  );
+}
+
+export default Header;
