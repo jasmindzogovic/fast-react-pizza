6dbf0cbc2ebeac9da29feccc6f26719a3b9f8fba
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-05 11:59:23 -0500

    added functionality to load order by id

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,14 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza</Link>
+
+      <SearchOrder />
+
       <p>Jasmin</p>
     </header>
   );
