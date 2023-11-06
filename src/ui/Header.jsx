254b2575ca4165c8cf824d14641cf7d2af03446a
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-06 14:26:57 -0500

    added more styling to components

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,18 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Jasmin</p>
+      <Username />
     </header>
   );
 }
