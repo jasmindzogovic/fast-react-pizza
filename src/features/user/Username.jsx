2d233a007d7465b1554cad4023f9fa01173dfbfc
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-06 23:37:14 -0500

    added more styling to components

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Jasmin</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jasmin</div>;
 }
 
 export default Username;
