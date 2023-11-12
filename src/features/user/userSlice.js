12b4604c599a7a9e62fe64ef3b99645786972bc4
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-12 11:58:00 -0500

    fixed geolocation bug

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -41,7 +41,7 @@ const userSlice = createSlice({
   },
   extraReducers: (builder) =>
     builder
-      .addCase(fetchAddress.pending, (state) => (state.status = "loading"))
+      .addCase(fetchAddress.pending, (state) => {(state.status = "loading")})
       .addCase(fetchAddress.fulfilled, (state, action) => {
         state.position = action.payload.position;
         state.address = action.payload.address;
